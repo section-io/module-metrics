0ca0f1d6af93b85d176cba5f60a6d414f40174e4
Glenn Slaven <glenn@section.io>
2019-06-25 06:07:16 +1000

    Add test checking that bytes and bytes_sent both get counted correctly

--- metrics_test.go
+++ metrics_test.go
@@ -175,6 +175,56 @@ func testCountersIncrease(t *testing.T, stdout *bytes.Buffer) {
 	_ = writer.Close()
 }
 
+func testBytesAndBytesSentAreRead(t *testing.T, stdout *bytes.Buffer) {
+
+	logs := []string{
+		`{"time":"2019-06-20T01:34:36+00:00","request_time":"0.070","hostname":"www.example.com","status":"200","bytes":"10","request":"GET /a/path HTTP/1.1","http_accept_encoding":"gzip","http_x_forwarded_proto":"https","http_upgrade":"-","http_connection":"-","body_bytes_sent":"0","upstream_label":"default","upstream_addr":"198.51.100.1:443","upstream_status":"304","upstream_request_connection":"","upstream_request_host":"in.example.com","upstream_header_time":"0.070","upstream_connect_time":"0.052","upstream_response_time":"0.070","upstream_response_length":"0","upstream_bytes_received":"288","upstream_http_content_type":"-","upstream_http_cache_control":"max-age=60","upstream_http_content_length":"-","upstream_http_content_encoding":"-","upstream_http_transfer_encoding":"-","sent_http_content_length":"-","sent_http_content_encoding":"-","sent_http_transfer_encoding":"-","section-io-id":"cf99df8057b93ec96c0ee1253ba4c309"}`,
+		`{"time":"2019-06-20T01:34:36+00:00","request_time":"0.069","hostname":"www.example.com","status":"200","bytes_sent":"20","request":"GET /a/path HTTP/1.1","http_accept_encoding":"gzip","http_x_forwarded_proto":"https","http_upgrade":"-","http_connection":"-","body_bytes_sent":"0","upstream_label":"default","upstream_addr":"198.51.100.1:443","upstream_status":"304","upstream_request_connection":"","upstream_request_host":"in.example.com","upstream_header_time":"0.069","upstream_connect_time":"0.052","upstream_response_time":"0.069","upstream_response_length":"0","upstream_bytes_received":"288","upstream_http_content_type":"-","upstream_http_cache_control":"max-age=60","upstream_http_content_length":"-","upstream_http_content_encoding":"-","upstream_http_transfer_encoding":"-","sent_http_content_length":"-","sent_http_content_encoding":"-","sent_http_transfer_encoding":"-","section-io-id":"451e230222237f722eb49324d47142f6"}`,
+	}
+
+	InitMetrics(moduleName)
+
+	writer, err := OpenWriteFifo(fifoFilePath)
+	if err != nil {
+		t.Errorf("OpenWriteFifo(%s) failed: %#v", fifoFilePath, err)
+	}
+
+	for _, line := range logs {
+		_, err := writer.Write([]byte(line + "\n"))
+		if err != nil {
+			t.Errorf("Error writing line: %#v", err)
+		}
+	}
+
+	//Give the reader loop time to finish
+	time.Sleep(time.Second * 1)
+
+	gathering, err := registry.Gather()
+	if err != nil {
+		t.Errorf("Error in prometheus Gather: %#v", err)
+	}
+
+	out := &bytes.Buffer{}
+	for _, mf := range gathering {
+		if _, err := expfmt.MetricFamilyToText(out, mf); err != nil {
+			t.Errorf("Error in expfmt.MetricFamilyToText: %#v", err)
+		}
+	}
+
+	actual := out.String()
+	expected := `test_module_http_request_count_total{hostname="www.example.com",status="200"} 2`
+	if !strings.Contains(actual, expected) {
+		t.Errorf("Output:\n%s\n does not contain expected %s", actual, expected)
+	}
+
+	expected = `test_module_http_bytes_total{hostname="www.example.com",status="200"} 30`
+	if !strings.Contains(actual, expected) {
+		t.Errorf("Output:\n%s\n does not contain expected %s", actual, expected)
+	}
+
+	_ = writer.Close()
+}
+
 func testP8sServer(t *testing.T, stdout *bytes.Buffer) {
 
 	logs := []string{
@@ -238,6 +288,7 @@ func TestReaderRunning(t *testing.T) {
 
 	t.Run("testLogsOutputEqualsInput", func(t *testing.T) { testLogsOutputEqualsInput(t, stdout) })
 	t.Run("testCountersIncrease", func(t *testing.T) { testCountersIncrease(t, stdout) })
+	t.Run("testBytesAndBytesSentAreRead", func(t *testing.T) { testBytesAndBytesSentAreRead(t, stdout) })
 	t.Run("testP8sServer", func(t *testing.T) { testP8sServer(t, stdout) })
 
 	teardown(t)
