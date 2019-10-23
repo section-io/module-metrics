21aca0ba1391a51f169fc588b081e162f54425c7
Glenn Slaven <glenn@section.io>
2019-10-24 08:48:08 +1100

    Sanitize the label name even if there is no value

--- metrics_test.go
+++ metrics_test.go
@@ -32,99 +32,98 @@ func TestCreateLogFifoFails(t *testing.T) {
 
 func TestUnknownLabelSanitize(t *testing.T) {
 	const expectedLabel = "some_other_field"
-	const expectedValue = "foobar"
-	actual, _ := sanitizeLabel("some_other_field", "foobar")
+	actual := sanitizeLabelName("some_other_field")
 	assert.Equal(t, expectedLabel, actual)
 }
 
 func TestSanitizeContentTypeLabelName(t *testing.T) {
 	const expectedLabel = "content_type_bucket"
-	actual, _ := sanitizeLabel("content_type", "text/html")
+	actual := sanitizeLabelName("content_type")
 	assert.Equal(t, expectedLabel, actual)
 }
 
 func TestSanitizeContentTypeHTML(t *testing.T) {
 	const expected = "html"
-	_, actual := sanitizeLabel("content_type", "text/html; charset=iso-8859-1")
+	actual := sanitizeLabelValue("content_type", "text/html; charset=iso-8859-1")
 	assert.Equal(t, expected, actual)
 
-	_, actual = sanitizeLabel("content_type", "text/html")
+	actual = sanitizeLabelValue("content_type", "text/html")
 	assert.Equal(t, expected, actual)
 }
 
 func TestSanitizeContentTypeImage(t *testing.T) {
 	const expected = "image"
-	_, actual := sanitizeLabel("content_type", "image/jpg")
+	actual := sanitizeLabelValue("content_type", "image/jpg")
 	assert.Equal(t, expected, actual)
 
-	_, actual = sanitizeLabel("content_type", "image/gif")
+	actual = sanitizeLabelValue("content_type", "image/gif")
 	assert.Equal(t, expected, actual)
 }
 
 func TestSanitizeContentTypeCSS(t *testing.T) {
 	const expected = "css"
-	_, actual := sanitizeLabel("content_type", "text/css")
+	actual := sanitizeLabelValue("content_type", "text/css")
 	assert.Equal(t, expected, actual)
 }
 
 func TestSanitizeContentTypeJavascript(t *testing.T) {
 	const expected = "javascript"
-	_, actual := sanitizeLabel("content_type", "text/javascript")
+	actual := sanitizeLabelValue("content_type", "text/javascript")
 	assert.Equal(t, expected, actual)
 
-	_, actual = sanitizeLabel("content_type", "application/javascript")
+	actual = sanitizeLabelValue("content_type", "application/javascript")
 	assert.Equal(t, expected, actual)
 }
 
 func TestSanitizeContentTypeEmpty(t *testing.T) {
 	const expected = ""
-	_, actual := sanitizeLabel("content_type", "")
+	actual := sanitizeLabelValue("content_type", "")
 	assert.Equal(t, expected, actual)
 
-	_, actual = sanitizeLabel("content_type", "-")
+	actual = sanitizeLabelValue("content_type", "-")
 	assert.Equal(t, expected, actual)
 }
 
 func TestSanitizeContentTypeOther(t *testing.T) {
 	const expected = "other"
-	_, actual := sanitizeLabel("content_type", "foobar")
+	actual := sanitizeLabelValue("content_type", "foobar")
 	assert.Equal(t, expected, actual)
 
-	_, actual = sanitizeLabel("content_type", "text/rtf")
+	actual = sanitizeLabelValue("content_type", "text/rtf")
 	assert.Equal(t, expected, actual)
 }
 
 func TestUnsanitizedLabel(t *testing.T) {
 	const expected = "fooooo3iwac"
-	_, actual := sanitizeLabel("some_unknown_type", " fooooo3iwac ")
+	actual := sanitizeLabelValue("some_unknown_type", " fooooo3iwac ")
 
 	assert.Equal(t, expected, actual)
 }
 
 func TestSanitizeNil(t *testing.T) {
 	const expected = ""
-	_, actual := sanitizeLabel("foo", nil)
+	actual := sanitizeLabelValue("foo", nil)
 
 	assert.Equal(t, expected, actual)
 }
 
 func TestSanitizeHostname(t *testing.T) {
 	const expected = "www.foo.com"
-	_, actual := sanitizeLabel("hostname", "www.foo.com")
+	actual := sanitizeLabelValue("hostname", "www.foo.com")
 
 	assert.Equal(t, expected, actual)
 }
 
 func TestSanitizeHostnameWithPort(t *testing.T) {
 	const expected = "www.foo.com"
-	_, actual := sanitizeLabel("hostname", "www.foo.com:80")
+	actual := sanitizeLabelValue("hostname", "www.foo.com:80")
 
 	assert.Equal(t, expected, actual)
 }
 
 func TestSanitizeHostnameWithSpaces(t *testing.T) {
 	const expected = "www.foo.com"
-	_, actual := sanitizeLabel("hostname", "   www.foo.com   ")
+	actual := sanitizeLabelValue("hostname", "   www.foo.com   ")
 
 	assert.Equal(t, expected, actual)
 }
@@ -132,19 +131,19 @@ func TestSanitizeHostnameWithSpaces(t *testing.T) {
 func TestSanitizeHostnameMissing(t *testing.T) {
 	const expected = ""
 
-	_, actual := sanitizeLabel("hostname", nil)
+	actual := sanitizeLabelValue("hostname", nil)
 	assert.Equal(t, expected, actual)
 
-	_, actual = sanitizeLabel("hostname", "-")
+	actual = sanitizeLabelValue("hostname", "-")
 	assert.Equal(t, expected, actual)
 
-	_, actual = sanitizeLabel("hostname", "    ")
+	actual = sanitizeLabelValue("hostname", "    ")
 	assert.Equal(t, expected, actual)
 }
 
 func TestSanitizeHostnameCasing(t *testing.T) {
 	const expected = "www.foo.com"
-	_, actual := sanitizeLabel("hostname", "WWw.FOo.COm")
+	actual := sanitizeLabelValue("hostname", "WWw.FOo.COm")
 
 	assert.Equal(t, expected, actual)
 }
@@ -152,16 +151,16 @@ func TestSanitizeHostnameCasing(t *testing.T) {
 func TestSanitizeHostnameInvalidChars(t *testing.T) {
 	const expected = ""
 
-	_, actual := sanitizeLabel("hostname", "www.fi$h.com")
+	actual := sanitizeLabelValue("hostname", "www.fi$h.com")
 	assert.Equal(t, expected, actual)
 
-	_, actual = sanitizeLabel("hostname", "%(+ ")
+	actual = sanitizeLabelValue("hostname", "%(+ ")
 	assert.Equal(t, expected, actual)
 }
 
 func TestSanitizeStatus(t *testing.T) {
 	const expected = "200"
-	_, actual := sanitizeLabel("status", "200")
+	actual := sanitizeLabelValue("status", "200")
 
 	assert.Equal(t, expected, actual)
 }
@@ -169,20 +168,20 @@ func TestSanitizeStatus(t *testing.T) {
 func TestSanitizeStatusInvalid(t *testing.T) {
 	const expected = ""
 
-	_, actual := sanitizeLabel("status", "220")
+	actual := sanitizeLabelValue("status", "220")
 	assert.Equal(t, expected, actual)
 
-	_, actual = sanitizeLabel("status", "foobar")
+	actual = sanitizeLabelValue("status", "foobar")
 	assert.Equal(t, expected, actual)
 
-	_, actual = sanitizeLabel("status", "220foo")
+	actual = sanitizeLabelValue("status", "220foo")
 	assert.Equal(t, expected, actual)
 
 }
 
 func TestSanitizeMaxLength(t *testing.T) {
 	const expected = "01234567890123456789012345678901234567890123456789012345678901234567890123456789"
-	_, actual := sanitizeLabel("hostname", "012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789")
+	actual := sanitizeLabelValue("hostname", "012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789")
 
 	assert.Equal(t, expected, actual)
 }
